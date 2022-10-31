3e2ce7982d08f83ccf28730c18bdf6157c735df5
Nolan <nolanwelch@outlook.com>
2022-10-31 14:13:39 -0400

    Add some definitions to main.ino

--- main/main.ino
+++ main/main.ino
@@ -1,4 +1,10 @@
-#define PULSESPERROTATION 20
+#include "RotaryEncoder.h"
+
+#define DT_PIN 3
+#define CLK_PIN 4
+#define PULSES_PER_ROTATION 20
+
+RotaryEncoder encoder = RotaryEncoder(DT_PIN, CLK_PIN, PULSES_PER_ROTATION);
 
 void setup() {
 	
