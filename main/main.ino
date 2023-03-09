0746d0c128fb536cde96aaa9fa1957f53033ab68
Nolan <nolanwelch@outlook.com>
2023-03-09 01:16:35 -0500

    Get code working (I think)

--- main/main.ino
+++ main/main.ino
@@ -1,12 +1,29 @@
-#define CLK_PIN 2
-#define DT_PIN 3
+#include <NeoPixelAnimator.h>
+#include <NeoPixelBrightnessBus.h>
+#include <NeoPixelBus.h>
+#include <NeoPixelBusLg.h>
+#include <NeoPixelSegmentBus.h>
 
-#define MOTOR_TIMEOUT_MS 60000
+#define LED_PIN 3 // forced by NeoPixelBus library
+// DMA methods use GPIO3
+// UART1 methods use GPIO2
+// UART0 methods use GPIO1
+#define LIGHT_TIMEOUT_MS 120000
+const uint16_t LED_COUNT = 41;
+#define COLOR_R 223
+#define COLOR_G 20
+#define COLOR_B 250
+RgbColor black(0);
+NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> strip(LED_COUNT);
+bool lightOn = false;
+
+const uint8_t CLK_PIN = D6;
+const uint8_t DT_PIN = D7;
 #define ENCODER_TIMEOUT_MS 5000
-#define PULSES_PER_ROTATION (float) 20
-#define ROTATIONS_TO_DROP (float) 10
-#define MAX_BRIGHTNESS (float) 255
-const float BRIGHTNESS_COEFF = MAX_BRIGHTNESS/(PULSES_PER_ROTATION*ROTATIONS_TO_DROP);
+#define PULSES_PER_ROTATION 20
+#define ROTATIONS_TO_LIGHT 5
+#define MAX_BRIGHTNESS 255
+const float BRIGHTNESS_COEFF = MAX_BRIGHTNESS/(float)(PULSES_PER_ROTATION*ROTATIONS_TO_LIGHT);
 
 bool lastClkState;
 bool currClkState;
@@ -14,41 +31,8 @@ volatile bool encoderUpdated = false;
 unsigned int pulses = 0;
 int lastPulses = 0;
 unsigned long lastEncoderMs = 0;
-unsigned long lastMotorMs = 0;
 
-void setup() {
-  Serial.begin(9600);
-  pinMode(CLK_PIN, INPUT);
-  pinMode(DT_PIN, INPUT);
-  lastClkState = digitalRead(CLK_PIN);
-  attachInterrupt(digitalPinToInterrupt(CLK_PIN), updateEncoder, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(DT_PIN), updateEncoder, CHANGE);
-  delay(1500);
-  Serial.println("Setup complete!");
-}
-
-void loop() {
-  //  encoder logic
-  if (encoderUpdated) {
-    encoderUpdated = false;
-    readEncoderUpdate();
-    int brightness = min(pulses*BRIGHTNESS_COEFF, MAX_BRIGHTNESS);
-    Serial.println(pulses);
-    Serial.print(brightness);
-    Serial.print("/255\n");
-  } else if (millis() - lastEncoderMs >= ENCODER_TIMEOUT_MS) {
-      pulses = 0;
-  }
-
-  //  mouth logic
-  if (pulses >= PULSES_PER_ROTATION*ROTATIONS_TO_DROP) {
-    openMouth();
-  } else if (millis() - lastMotorMs >= MOTOR_TIMEOUT_MS) {
-    closeMouth();
-  }
-}
-
-void updateEncoder() {
+void ICACHE_RAM_ATTR updateEncoder() {
   encoderUpdated = true;
 }
 
@@ -61,10 +45,63 @@ void readEncoderUpdate() {
   lastClkState = currClkState;
 }
 
-void openMouth() {
-  
+void setup() {
+  Serial.begin(9600); // MUST leave this in or LED strip will not work!
+  while(!Serial); // remove when done testing
+  Serial.println("Beginning setup...");
+  pinMode(CLK_PIN, INPUT);
+  pinMode(DT_PIN, INPUT);
+  lastClkState = digitalRead(CLK_PIN);
+  Serial.println("Set up pins!");
+  attachInterrupt(digitalPinToInterrupt(CLK_PIN), updateEncoder, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(DT_PIN), updateEncoder, CHANGE);
+  Serial.println("Attached interrupts!");
+  strip.Begin();
+  Serial.println("Started light strip!");
+  for (int i = 0; i < LED_COUNT; i++) {
+    strip.SetPixelColor(i, black);
+  }
+  Serial.println("Assigned color to light strip!");
+  strip.Show();
+  Serial.println("Showing light strip!");
+  delay(1500);
+  Serial.println("Setup complete!");
+  Serial.println(PULSES_PER_ROTATION*ROTATIONS_TO_LIGHT); // temp
 }
 
-void closeMouth() {
-  
+void loop() {
+  //  encoder logic
+  if (encoderUpdated) {
+    encoderUpdated = false;
+    readEncoderUpdate();
+    if (!lightOn) {
+      int brightness = min(pulses*BRIGHTNESS_COEFF, (float)MAX_BRIGHTNESS);
+      RgbColor color(brightness*COLOR_R, brightness*COLOR_G, brightness*COLOR_B);
+      for (int i = 0; i < LED_COUNT; i++) {
+        strip.SetPixelColor(i, color);
+      }
+      strip.Show();
+      Serial.println(pulses);
+    }
+  } else if (pulses > 0 && millis() - lastEncoderMs >= ENCODER_TIMEOUT_MS) {
+      pulses = 0;
+      for (int i = 0; i < LED_COUNT; i++) {
+        strip.SetPixelColor(i, black);
+      }
+      strip.Show();
+      Serial.println("Pulses and light reset");
+  }
+
+  //  light logic
+  if (!lightOn && pulses >= PULSES_PER_ROTATION*ROTATIONS_TO_LIGHT) {
+    lightOn = true;
+    Serial.println("Light on");
+  } else if (lightOn && millis() - lastEncoderMs >= LIGHT_TIMEOUT_MS) {
+    for (int i = 0; i < LED_COUNT; i++) {
+      strip.SetPixelColor(i, black);
+    }
+    strip.Show();
+    lightOn = false;
+    Serial.println("Light reset");
+  }
 }
